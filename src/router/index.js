f9fd48c6bbd150999c500909e49efa69c96dc527
Newcodemaster <842458727@qq.com>
2021-01-20 16:26:07 +0800

    init shop

---
+++ src/router/index.js
@@ -0,0 +1,55 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Msite from '../pages/Msite/Msite.vue'
+import Search from '../pages/Search/Search.vue'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
+import Login from '../pages/Login/Login.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/msite',
+      name: 'Msite',
+      component: Msite,
+      meta:{
+        showFooter: true
+      }
+    },
+    {
+      path: '/',
+      redirect: '/msite'
+    },
+    {
+      path: '/order',
+      name: 'Order',
+      component: Order,
+      meta:{
+        showFooter: true
+      }
+    },
+    {
+      path: '/search',
+      name: 'Search',
+      component: Search,
+      meta:{
+        showFooter: true
+      }
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile,
+      meta:{
+        showFooter: true
+      }
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    }
+  ]
+})
